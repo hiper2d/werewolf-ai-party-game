97c104391f723046eacbd52a119b5c9b9ec84ffc
hiper2d <hiper2d@gmail.com>
2024-07-20 19:17:54 -0400

    Got working version of service worker based auth no both client and server

--- werewolf-client/firebase/server.ts
+++ werewolf-client/firebase/server.ts
@@ -3,10 +3,11 @@ import serviceAccount from "./serviceAccount.json";
 import {Firestore, getFirestore } from "firebase-admin/firestore";
 import {headers} from "next/headers";
 import {initializeServerApp} from "@firebase/app";
-import {getAuth} from "firebase/auth";
+import {Auth, getAuth} from "firebase/auth";
 import {firebaseConfig} from "@/firebase/firebase-keys";
 
 let db: Firestore | undefined = undefined;
+let auth: Auth | undefined = undefined;
 const currentApps = getApps();
 if (currentApps.length <= 0) {
     if (process.env.NEXT_PUBLIC_APP_ENV === 'emulator') {
@@ -22,6 +23,8 @@ if (currentApps.length <= 0) {
 async function getAuthenticatedAppForUser() {
     const idToken = headers().get("Authorization")?.split("Bearer ")[1];
 
+    // todo: This doesn't work with Auth Emulator
+    // I haven't found yet a way to configure auth in the service worker to use emulator auth service instead of the real one
     const firebaseServerApp = initializeServerApp(
         firebaseConfig,
         idToken
@@ -31,9 +34,10 @@ async function getAuthenticatedAppForUser() {
             : {}
     );
 
-    const auth = getAuth(firebaseServerApp);
-    await auth.authStateReady();
-
+    if (!auth) {
+        auth = getAuth(firebaseServerApp);
+        await auth.authStateReady();
+    }
     return { firebaseServerApp, currentUser: auth.currentUser };
 }
 
