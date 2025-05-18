4d77dcf46c87dc2d3f1c483fffe620cf329bdf06
hiper2d <hiper2d@gmail.com>
2025-05-17 21:33:48 -0400

    Fixed auth

--- werewolf-client/app/api/auth/[...nextauth]/route.ts
+++ werewolf-client/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,2 @@
-import NextAuth from 'next-auth';
-import { authOptions } from '../auth-options';
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers
